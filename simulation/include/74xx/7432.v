d4c45a9c970e112fea69e6ce127b3505b6c94596
Alvaro Polo <apoloval@gmail.com>
2018-03-17 19:20:09 +0100

    Add HC and HCT versions of 7432

--- simulation/include/74xx/7432.v
+++ simulation/include/74xx/7432.v
@@ -7,4 +7,12 @@ module ic7432_or #(parameter TPLH = 15, TPHL = 22)(input wire a, b, output wire 
     assign #(TPLH, TPHL) y = a | b;
 endmodule
 
+module ic74hc32_or(input wire a, b, output wire y);
+    ic7432_or #(.TPLH(18), .TPHL(18)) delegate(a, b, y);
+endmodule
+
+module ic74hct32_or(input wire a, b, output wire y);
+    ic7432_or #(.TPLH(24), .TPHL(24)) delegate(a, b, y);
+endmodule
+
 `endif
